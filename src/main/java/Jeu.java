7230469a695074957d09b1cccff5392f80bfd251
Tsipora Enkaoua <tsipoenkaoua@gmail.com>
2023-09-27 20:32:11 +0200

    essagedeuxieme commit

--- src/main/java/Jeu.java
+++ src/main/java/Jeu.java
@@ -6,6 +6,7 @@ public class Jeu {
 	
   public static void main(String[] args) {
     jeu();
+    replay();
   }
 
   
@@ -18,30 +19,39 @@ public class Jeu {
 	  String[] messageTooHigh = {"Too high!", "The number is lower", "Take another try, it's too high","Decrease your number","Too bad, the number was lower"};
 	  String[] messageTooLow = {"Too low!", "The number is higher", "Take another try, it's too low","Increase your number","Too bad, the number was higher"};
 	  
-	while(counter<5) {
+	  while(counter<5) {
 		System.out.println("Devinez le nombre : ");
 	    //penser a gerer l'erreur qd numberGame n'est pas un chiffre************************
 		  int numberUser = sc.nextInt();
 		  
 		  if(numberGame==numberUser){
 		    message= "Félicitation! Vous avez trouvé le nombre !";
-        break;
+		    break;
 		  }else {
 			  counter++;
-	       if(numberGame<numberUser){
-	    	   message = messageTooHigh[counter];
-		    }else{
-		    	 message = messageTooLow[counter];
-	       }
-        System.out.println(message);
-		  }
-	  
+			  if(numberGame<numberUser){
+				 message = messageTooHigh[counter-1];
+			  }else{
+		    	 message = messageTooLow[counter-1]; 
+			  }
+			  System.out.println(message);
+		  }  
+	  } 
+    
 	}
-    
-    
-}
    
   
+	//Demande à l'utilisateur s'il veur rejouer
+	public static void replay() {
+		Scanner sc = new Scanner(System.in);
+		 System.out.println("Voulez vous rejouer? oui=0 / non=1");
+		  int game = sc.nextInt();
+		  if(game==0) {
+			  jeu();
+		  }else {
+			  System.out.println("Au plaisir de vous revoir!");
+		  }
+	}
 
    //Génération d'un nombre entre 1 et 100 aléatoirement
   public static int randomNumber(){
